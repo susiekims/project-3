645e30609282326694762b22c68eb17f07a3bf67
susiekims <susie.kim@hotmail.com>
2018-07-15 18:36:36 -0400

    added mapping

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,13 +1,13 @@
-
-// PSEUDOCODE
-// ask user what they are eating
-// ask user for price range
-// return random wine within those parameters
-
-
 const wineApp = {}
 
 wineApp.wineList = [];
+
+wineApp.pageNumber = 1;
+
+wineApp.productID;
+
+let searchFood ;
+let searchPrice ;
 
 wineApp.getWines = (food, price, pageNumber) => {
 	$.ajax({
@@ -20,39 +20,114 @@ wineApp.getWines = (food, price, pageNumber) => {
 		data: {
 			per_page: 100,
 			page: pageNumber,
+			q: 'wine',
+			order: 'price_in_cents.asc'
 		}
 		}).then(function(res) {
 			
 			const products = res.result;
-	
+
+			searchFood = $('#food').val();
+			searchPrice = $('#price').val();
+
+			// console.log(products);
+
+		if (products.length === 100 && products[99].price_in_cents < price) {
+			wineApp.pageNumber++;
+			// console.log(wineApp.pageNumber);
+			wineApp.getWines(searchFood, searchPrice, wineApp.pageNumber);
 			const filteredWineList = products.filter((product) => {
 				return product.serving_suggestion != null &&
+					   product.image_url != null &&
     				   product.primary_category === 'Wine' &&
-    				   product.price_in_cents <= price &&
     				   product.price_in_cents >= price - 400 &&
-    				   product.serving_suggestion.indexOf(`${food}`) !== -1;
+    				   product.serving_suggestion.indexOf(food) != -1 &&
+    				   product.package_unit_volume_in_milliliters === 750;
+
 			});
 
 			if (filteredWineList.length > 0) {
+
 				wineApp.wineList.push(filteredWineList);
-			}
-		});
+				// console.log(filteredWineList);
+			}	
+		} else {
+			console.log('no more wine!')
+			wineApp.flattenArray();
+		}
+	});
 }
 
-wineApp.getAllWines = () => {
-	for (let i = 1; i < 5; i ++) {
-		const searchFood = $('#food').val();
-		const searchPrice = $('#price').val();
-		wineApp.getWines(searchFood, searchPrice, i);
-	}
-	console.log(wineApp.wineList);
+wineApp.flattenArray = () => {
+	const flattenedList = wineApp.wineList.reduce(function(a, b) {
+  			return a.concat(b);
+		});
+		// console.log(flattenedList);
+		wineApp.pickRandomWine(flattenedList);
+}
+
+wineApp.pickRandomWine = (arr) => {
+	const theWinner = arr[Math.floor(Math.random()*arr.length)];
+	console.log(theWinner);
+	wineApp.displayWine(theWinner);
+}
+
+wineApp.displayWine = (wine) => {
+	$('#loader').removeClass("loader");
+	$('#locations').toggle(true);
+	const $name = $('<h2>').text(wine.name);
+	const $producer = $('<h3>').text(wine.producer_name);
+	const $varietal = $('<p>').text(wine.varietal);
+	const $taste = $('<p>').text(wine.tasting_note);
+	const $imgURL = $('<img>').attr('src', wine.image_url);
+	$('#wine').append($name, $producer, $varietal, $taste, $imgURL);
+	wineApp.productID = wine.id;
+}
+
+
+wineApp.getUserCoords = () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        const userLat = position.coords.latitude;
+        const userLng = position.coords.longitude;
+        wineApp.getStores(wineApp.productID, userLat, userLng);
+    });
+}
+
+wineApp.getStores = (idNumber, latitude, longitude) => {
+	$.ajax({
+		url: 'https://lcboapi.com/stores',
+		method: 'GET',
+		dataType:'jsonp',
+		headers: {
+			'Authorization': 'Token MDoxZmYyNzU4MC03NWE3LTExZTgtYmVmMy0zYmY0Njk4ZDI5YTA6cHhBRGw5RWFmMjZuQk1PcWJhZzg1Vm5OMzBLaWpkTnNqU0ZW'
+			},
+		data: {
+			product_id: idNumber,
+			per_page: 3,
+			lat: latitude,
+			lng: longitude
+		}
+		}).then(function(res) {	
+			const stores = res.result;
+			console.log(stores);
+		});
 }
 
 
 wineApp.init = () => {
-	$('form').on('submit', function(e) {
+	$('#wineButton').on('click', function(e) {
 		e.preventDefault();
-		wineApp.getAllWines();
+		$('#loader').addClass("loader");
+		wineApp.pageNumber = 1;
+		wineApp.wineList = [];
+		$('#wine').empty();
+		searchFood = $('#food').val();
+		searchPrice = $('#price').val();
+		console.log(searchFood + searchPrice);
+		wineApp.getWines(searchFood, searchPrice, wineApp.pageNumber);
+	});	
+	$('#storeButton').on('click', function(e) {
+		wineApp.getUserCoords();
 	});
 }
 
@@ -61,6 +136,5 @@ $(function() {
 });
 
 
-// EXTRA FEATURES
-// ask user for location
-// limit result to wine in stock at close LCBOs
+// 1. how to loop through function on how many pages exist
+// 2. how to run flattenArray after getAllWines (promises?)
